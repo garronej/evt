ed3495c9aa909df90ce5b448f4cc3f49b93a3bd3
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-02-07 06:07:38 +0100

    first iteration of readme

--- src/lib/Observable.ts
+++ src/lib/Observable.ts
@@ -11,11 +11,35 @@ export class ObservableImpl<T> implements Observable<T> {
 
     public readonly evtChange = new SyncEvent<T>();
 
+    //NOTE: Not really readonly but we want to prevent user from setting the value
+    //manually and we cant user accessor because we target es3.
+    public readonly value!: T;
+
     constructor(
-        public value: T,
+        initialValue: T,
         private areSame: (oldValue: T, newValue: T) => boolean = (oldValue, newValue) => oldValue === newValue
     ) {
+        this.overwriteReadonlyValue(initialValue);
     }
+
+
+    private overwriteReadonlyValue = (() => {
+
+        const propertyDescriptor: PropertyDescriptor = {
+            "configurable": true,
+            "enumerable": true,
+            "writable": false,
+        };
+
+        return (newValue: T) => {
+
+            propertyDescriptor.value = newValue;
+
+            Object.defineProperty(this, "value", propertyDescriptor);
+
+        };
+
+    })();
 
     public onPotentialChange(newValue: T): void {
 
@@ -23,7 +47,8 @@ export class ObservableImpl<T> implements Observable<T> {
             return;
         }
 
-        this.value = newValue;
+        this.overwriteReadonlyValue(newValue);
+
 
         this.evtChange.post(this.value);
 
