601a70979c6d5025850253f007a6ce1e782b2204
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-04-09 15:01:28 +0200

    Export EvtLike

--- dist/lib/types/helper/index.d.ts
+++ dist/lib/types/helper/index.d.ts
@@ -2,4 +2,4 @@ export { SwapEvtType } from "./SwapEvtType";
 export { ToPostableEvt } from "./ToPostableEvt";
 export { ToNonPostableEvt } from "./ToNonPostableEvt";
 export { UnpackCtx } from "./UnpackCtx";
-export { UnpackEvt } from "./UnpackEvt";
+export { UnpackEvt, EvtLike } from "./UnpackEvt";
