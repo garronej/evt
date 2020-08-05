507c24e42d0212cfa49d5c88e5ba5dc8a08a99c3
denoify_ci <denoify_ci@github.com>
2020-08-05 03:31:02 +0000

    Enable shorter import paths [automatic]

---
+++ lib/Evt.asPostable.ts
@@ -0,0 +1,12 @@
+
+import { ToPostableEvt } from "./types/helper/ToPostableEvt.ts";
+type EvtLike<T>= import("./types/helper/UnpackEvt.ts").EvtLike<T>;
+
+/** 
+ * https://docs.evt.land/api/evt/aspostable 
+ * ⚠ UNSAFE ⚠ - Please refer to documentation before using.
+ * */
+export function asPostable<E extends EvtLike<any>>(evt: E): ToPostableEvt<E>{
+    return evt as any;
+}
+
