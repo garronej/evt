8fd85f325e8b8e2ddc713c3cae037bf5f61265f2
Joseph Garrone <joseph.garrone.gj@gmail.com>
2021-03-01 11:48:39 +0100

    Fix exports in evt/tools/typeSafety

--- src/tools/typeSafety/AsyncReturnType.ts
+++ src/tools/typeSafety/AsyncReturnType.ts
@@ -1,3 +1,4 @@
 
 /** Extract the return type of an async function */
-export type AsyncReturnType<T extends (...args: any) => Promise<any>> = T extends (...args: any) => Promise<infer R> ? R : never;
+export type AsyncReturnType<T extends ((...args: any) => Promise<any>) | null | undefined | false | ""> = 
+    T extends (...args: any) => Promise<infer R> ? R : never;
