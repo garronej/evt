4344e19c7714f10f41e16fd9c5a3ef991a65b704
garronej <joseph.garrone.gj@gmail.com>
2021-05-26 18:44:55 +0200

    Fix bug with useRerenderOnStateChange

--- src/hooks/useRerenderOnStateChange.ts
+++ src/hooks/useRerenderOnStateChange.ts
@@ -1,13 +1,14 @@
+import { Polyfill as WeakSet } from "minimal-polyfills/WeakSet";
+import { Evt } from "evt";
+import { useEvt } from "evt/hooks";
 
 import * as React from "react";
 const { useReducer } = React;
 
-import { Evt } from "../lib";
-import { useEvt } from "./useEvt";
 
-type CtxLike<Result = any> = import("../lib/types/interfaces/CtxLike").CtxLike<Result>;
+type CtxLike<Result = any> = import("evt/lib/types/interfaces/CtxLike").CtxLike<Result>;
 interface HandlerLike { ctx: CtxLike };
-type Pipe<Cb = () => void> = (ctx: CtxLike, cb?: Cb) => import("../lib/Evt.merge").EvtLike<any>;
+type Pipe<Cb = () => void> = (ctx: CtxLike, cb?: Cb) => import("evt/lib/Evt.merge").EvtLike<any>;
 
 interface StatefulReadonlyEvtLike {
     evtChange: {
@@ -28,30 +29,37 @@ interface StatefulReadonlyEvtLike {
     }
 };
 
+const ctxs = new WeakSet<any>();
+
 /**
  * https://docs.evt.land/api/react-hooks
  * 
  * To use StatefulEvt as react component state.
  * */
-export function useRerenderOnStateChange(...evts: StatefulReadonlyEvtLike[]): void {
+export function useRerenderOnStateChange(evts: StatefulReadonlyEvtLike[]): void {
 
     const [, forceUpdate] = useReducer(x => x + 1, 0);
 
     useEvt(
         ctx => {
 
+            ctxs.add(ctx);
+
             evts.forEach(evt => {
 
-                const attach = () => evt.evtChange.toStateless(ctx).attach(forceUpdate);
+                const attach = () => evt.evtChange.toStateless(ctx).attach(() => forceUpdate());
 
                 attach();
 
                 //NOTE: We do all this funny business because we want to ensure that the handler
                 //that triggers the re-render is always the last handler to be invoked.
                 //What we do is we detach our handler when an other is added and synchronously re-attach it
+                //Using the ctxs WeakMap we avoid infinite loop, each handler of useRerenderOnStateChange
+                //trying to be the first. (happens when useRerenderOnStateChange is called multiple time on
+                //an event)
                 Evt.merge(
                     [
-                        evt.evtChange.evtAttach.pipe(ctx, handler => handler.ctx !== ctx),
+                        evt.evtChange.evtAttach.pipe(ctx, handler => !ctxs.has(handler.ctx)),
                         ...[
                             evt,
                             evt.evtChangeDiff,
@@ -66,9 +74,11 @@ export function useRerenderOnStateChange(...evts: StatefulReadonlyEvtLike[]): vo
 
                 });
 
+
             });
 
         },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
         evts
     );
 
