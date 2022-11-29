e7ff4185f66fbc68c650b5c305f4a0816fc91f14
actions <actions@github.com>
2022-11-29 10:53:06 +0000

    Adding deno distribution files and moving files from /dist to /

---
+++ lib/types/interfaces/Postable.d.ts
@@ -0,0 +1,14 @@
+export interface Postable<T> {
+    /** https://docs.evt.land/api/evt/post */
+    postAsyncOnceHandled(data: T): number | Promise<number>;
+    /**
+     * Returns post count
+     * */
+    post(data: T): number;
+    /**
+     * Post and wait for all async handler's callbacks have resolved.
+     *
+     * https://docs.evt.land/api/evt/post#evt-postandwait-data-promise-less-than-void-greater-than
+     */
+    postAndWait(data: T): Promise<void>;
+}
---
+++ test/test85.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var lib_1 = require("../lib");
+var assert_1 = require("tsafe/assert");
+(0, assert_1.assert)(lib_1.Evt.name === "Evt");
+(0, assert_1.assert)(lib_1.Ctx.name === "Ctx");
+console.log("PASS");
+//# sourceMappingURL=test85.js.map
