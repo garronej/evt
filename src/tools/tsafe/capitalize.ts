93a54801cdb34ac5e554faec0eb3a6614387479c
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-11-18 18:52:49 +0100

    Remove leftover file

--- src/tools/tsafe/capitalize.ts
+++
@@ -1,4 +0,0 @@
-/** @see <https://docs.tsafe.dev/capitalize> */
-export function capitalize<S extends string>(str: S): Capitalize<S> {
-    return (str.charAt(0).toUpperCase() + str.slice(1)) as any;
-}
